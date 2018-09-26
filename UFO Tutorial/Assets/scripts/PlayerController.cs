61aee3555055d842df98ee65dda141ab4346cebe
kirsten-culp <43148594+kirsten-culp@users.noreply.github.com>
2018-09-26 17:46:07 -0400

    Final

--- UFO Tutorial/Assets/scripts/PlayerController.cs
+++ UFO Tutorial/Assets/scripts/PlayerController.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
+
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody2D rb2d;
+    private int count;
 
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        winText.text = "";
+        SetCountText();
 	}
 	
 	// Update is called once per frame
@@ -17,6 +25,10 @@ public class PlayerController : MonoBehaviour {
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce(movement * speed);
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
 	}
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -24,6 +36,17 @@ public class PlayerController : MonoBehaviour {
       if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You win!";
         }
     }
 }
